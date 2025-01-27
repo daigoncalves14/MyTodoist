0a76f85cbae0c5d7c778798e79d5fa716b570efc
daiane goncalves <daiane.goncalves@ab-inbev.com>
2025-01-26 22:07:27 -0300

    Initial commit

---
+++ my-todoist/src/main/java/com/example/mytodoist/MyTodoistApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytodoist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodoistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTodoistApplication.class, args);
+    }
+
+}
---
+++ my-todoist/src/test/java/com/example/mytodoist/MyTodoistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytodoist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
